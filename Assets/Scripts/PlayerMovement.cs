4512cd72d3a946e383ddd44b27a5b97080453d55
lucas-renaudie <lucas.renaudie@mail.mcgill.ca>
2025-01-21 18:42:16 +0100

    Movement animations
    Idle, run and jump animations (and disabled double jumping)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -5,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
     public float jumpHeight = 10;
     public float horizontalSpeed = 10;
     public Rigidbody2D myBody;
+    private Animator anim;
+    private bool grounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+        myBody.linearVelocity = new Vector2(horizontalInput * horizontalSpeed, myBody.linearVelocity.y);
 
         if (horizontalInput > 0) {
             transform.localScale = new Vector3(-1, 1, 1);
@@ -24,10 +28,24 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            myBody.linearVelocity = Vector2.up * jumpHeight;
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) {
+            Jump();
         }
 
-        myBody.linearVelocity = new Vector2(horizontalInput * horizontalSpeed, myBody.linearVelocity.y);
+        anim = GetComponent<Animator>();
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("Grounded", grounded);
+    }
+
+    private void Jump() {
+        myBody.linearVelocity = Vector2.up * jumpHeight;
+        anim.SetTrigger("Jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground") {
+            grounded = true;
+        }
     }
 }
