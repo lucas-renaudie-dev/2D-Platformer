8a8f04a0faffacf590ef7e161a8e50a596a57c1d
lucas-renaudie <lucas.renaudie@mail.mcgill.ca>
2025-01-23 18:59:50 +0100

    Coin, Spikes and Ghost, Death
    Animations for ghost and coin, animation for player death, game over logic for player death

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,11 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public bool isAlive = true;
+    public LogicManager logic;
     public float jumpHeight = 10;
     public float horizontalSpeed = 10;
     public Rigidbody2D myBody;
@@ -12,29 +15,36 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        myBody.linearVelocity = new Vector2(horizontalInput * horizontalSpeed, myBody.linearVelocity.y);
+        if (isAlive) {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            myBody.linearVelocity = new Vector2(horizontalInput * horizontalSpeed, myBody.linearVelocity.y);
 
-        if (horizontalInput > 0) {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-        if (horizontalInput < 0) {
-            transform.localScale = new Vector3(1, 1, 1);
+            if (horizontalInput > 0) {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            if (horizontalInput < 0) {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && grounded) {
+                Jump();
+            }
+
+            anim = GetComponent<Animator>();
+            anim.SetBool("run", horizontalInput != 0);
+            anim.SetBool("Grounded", grounded);
+            anim.SetBool("dead", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && grounded) {
-            Jump();
+        else {
+            anim.SetBool("dead", true);
         }
-
-        anim = GetComponent<Animator>();
-        anim.SetBool("Run", horizontalInput != 0);
-        anim.SetBool("Grounded", grounded);
     }
 
     private void Jump() {
