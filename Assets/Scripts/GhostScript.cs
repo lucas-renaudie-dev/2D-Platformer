4c20704bcec295e732f03ea778f93948dfe3e473
lucas-renaudie <lucas.renaudie@mail.mcgill.ca>
2025-01-24 02:17:09 +0100

    Ghost Movement
    Left to Right movement loop

--- Assets/Scripts/GhostScript.cs
+++ Assets/Scripts/GhostScript.cs
@@ -1,9 +1,14 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class GhostScript : MonoBehaviour
 {
     public LogicManager logic;
     public GameObject player;
+    private float timer = 0;
+    public float maxTime = 1.5f;
+    private float ghostDirection = -1;
+    public float ghostSpeed = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +20,21 @@ public class GhostScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer < maxTime) {
+            timer += Time.deltaTime;
+        }
+        else {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            timer = 0;
+            ghostDirection = -ghostDirection;
+        }
+
+        if (ghostDirection == -1) {
+            transform.position = transform.position + (Vector3.left * ghostSpeed) * Time.deltaTime;
+        }
+        else {
+            transform.position = transform.position + (Vector3.right * ghostSpeed) * Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
