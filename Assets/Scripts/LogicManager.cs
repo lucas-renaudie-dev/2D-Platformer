afb90753bdb66e8e3961ee527f0e0b06aff5302b
lucas-renaudie <lucas.renaudie@mail.mcgill.ca>
2025-01-27 04:17:03 +0100

    Fixed camera, added checkpoints

--- Assets/Scripts/LogicManager.cs
+++ Assets/Scripts/LogicManager.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LogicManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void gameOver() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
