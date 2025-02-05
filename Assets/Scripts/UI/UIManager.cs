ec82b9c108575b05df148c76c575492f8951af0b
lucas-renaudie <lucas.renaudie@mail.mcgill.ca>
2025-02-05 02:39:58 +0100

    Implemented selector for Game Over screen

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,6 +5,10 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private AudioClip gameOverSound;
 
+    private void Awake() {
+        gameOverScreen.SetActive(false);
+    }
+    
     public void GameOver() {
         gameOverScreen.SetActive(true);
         SoundManager.instance.PlaySound(gameOverSound);
